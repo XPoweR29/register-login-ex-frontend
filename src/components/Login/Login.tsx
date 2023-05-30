8e2268a39945cdfd21b08df252fcc34fb30dda8e
XPoweR29 <xpower-29@wp.pl>
2023-05-30 17:38:47 +0200

    Added user state and handleChange into Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,18 +1,37 @@
+import React, { useState } from 'react';
 import styles from './Login.module.scss';
 
 export const Login = () => {
+    const [user, setUser] = useState({
+        email: '',
+        pwd: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser(user => ({
+            ...user,
+            [e.target.name]: e.target.value,
+        })); 
+
+    }
+
+    const sendForm = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(user);
+    }
+
     return (
         <div className={styles.wrapper}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={sendForm}>
                 
                 <label htmlFor="email">Email</label>
-                <input id='email' type="email" />
+                <input id='email' name='email' type="email" value={user.email} onChange={handleChange}/>
 
                 <label htmlFor="pwd">Password</label>
-                <input id='pwd' type="password" />
+                <input id='pwd' name='pwd' type="password" value={user.pwd} onChange={handleChange} />
 
-                <button className={styles.loginBtn}>Log in</button>
-                <button className={styles.registerBtn}>Don't have an account? Register here</button>
+                <button type='submit' className={styles.loginBtn}>Log in</button>
+                <button className={styles.switchRegisterBtn}>Don't have an account? Sign in</button>
             </form> 
         </div>
     );
