18f5045d515fe6d62dc51faa3d41bd54e47ffe95
XPoweR29 <xpower-29@wp.pl>
2023-05-30 17:39:04 +0200

    Created Register component

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import styles from './Register.module.scss';
+
+export const Register = () => {
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        pwd: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser(user => ({
+            ...user,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    const sendForm = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(user);
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <form className={styles.form} onSubmit={sendForm}>
+
+                <label htmlFor="username">Username</label>
+                <input id='username' name='username' type="text" value={user.username} onChange={handleChange}/>
+
+                <label htmlFor="email">Email</label>
+                <input id='email' name='email' type="email" value={user.email} onChange={handleChange}/>
+
+                <label htmlFor="pwd">Password</label>
+                <input id='pwd' name='pwd' type="password" value={user.pwd} onChange={handleChange}/>
+
+                <button type='submit' className={styles.registerBtn}>Sign in</button>
+                <button className={styles.switchLoginBtn}>Already have an account? Log in</button>
+
+            </form>
+        </div>
+    );
+};
