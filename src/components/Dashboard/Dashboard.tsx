559f8e20c8bd3931976fa605f8d10e821f2b5a1d
XPoweR29 <xpower-29@wp.pl>
2023-06-04 13:49:17 +0200

    Added lougout function and action button

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,19 +1,19 @@
 import { useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { Navigate, useNavigate } from 'react-router';
 import { getAuthorization } from '../../utlis/getAuthorization';
 import { refreshAccessToken } from '../../utlis/refreshAccessToken';
 import { AppContext } from '../Common/Contexts/AppContext';
 import styles from './Dashboard.module.scss';
 
 export const Dashboard = () => {
-    const {userData} = useContext(AppContext)!;
+    const {userData, loggedIn, setLoggedIn} = useContext(AppContext)!;
     const navigate = useNavigate();
 
     useEffect(() => {
         const getAuth = async () => {
             try {
                 const res = await refreshAccessToken();
-                console.log(await getAuthorization(res.newAccessToken));
+                await getAuthorization(res.newAccessToken);
             } catch(err) {
                 console.error(err);
                 navigate('/', {replace: true});
@@ -21,18 +21,32 @@ export const Dashboard = () => {
         }
 
         const interval = setInterval(getAuth, 10000);
-
         return () => {
             clearInterval(interval);
         }
-
     }, []);
 
-   return (
-    <div className={styles.wrapper}>
-        <h1>Witaj {userData.username}</h1>
-        <hr />
-        <p className={styles.content}>To jest dashboard twojego profilu!</p>
-    </div>
-   ) 
+    const logout = async() => {
+        const rawRes = await fetch('http://localhost:3001/logout', {
+            method: 'DELETE',
+            credentials: 'include',
+        });
+        setLoggedIn(false);
+        // navigate('/', {replace: true});
+    };
+
+    if(loggedIn) {
+        return (
+         <div className={styles.wrapper}>
+             <h1>Witaj {userData.username}</h1>
+             <hr />
+             <p className={styles.content}>To jest dashboard twojego profilu!</p>
+     
+             <button onClick={logout} className={styles.logoutBtn}>Log out</button>
+         </div>
+        ) 
+    } 
+    else {
+        return <Navigate to='/' replace/>;
+    }
 }
